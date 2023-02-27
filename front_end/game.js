28a2a1f8f2fab94025561695073f2be97e78bb0d
Carl Poppa <poppacalypse@gmail.com>
2023-02-27 11:09:04 +0800

    build maps

--- front_end/game.js
+++ front_end/game.js
@@ -13,13 +13,13 @@ loadSprite("wall_mid", "/sprites/wall_mid.png");
 loadSprite("wall_right", "/sprites/wall_right.png");
 loadSprite("wall_fountain", "/sprites/wall_fountain.png", {
   sliceX: 3,
-  anims: {
-    idle: { from: 0, to: 2, speed: 5, loop: true }
+  anims: { // (anims in plural)
+    idle: { from: 0, to: 2, speed: 5, loop: true } // get frames 0-2, at speed of 5 frames per second
   }
 });
 
 
-scene("play", () => {
+scene("play", ({level}) => {
   // add background 10x10
   addLevel(
     [
@@ -40,7 +40,54 @@ scene("play", () => {
       // if you see a space, find a Sprite called 'floor'. then get a random frame from 0-8
       " ": () => [sprite("floor", { frame: ~~rand(0, 8) }),],
     }
-  )
+  );
+
+  const mapConfig = {
+    width: 16,
+    height: 16,
+    // solid means you can't move through it. "wall" at end is a tag, can be named anything
+    l: () => [sprite ("wall_left"), area(), solid(), "wall"], 
+    r: () => [sprite ("wall_right"), area(), solid(), "wall"],
+    w: () => [sprite ("wall_mid"), area(), solid(), "wall"],
+    // fountain is an animation, so must specify type - idle or run (anim in singular)
+    f: () => [
+      sprite ("wall_fountain", { anim: "idle" }), 
+      area(), 
+      solid(), 
+      "wall",
+    ],
+  };
+
+  // List of maps
+  const matrix = [
+    [
+      "lwwwffwwwr",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "lwwwwwwwwr",
+    ],
+    [
+      "lffffffffr",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "l        r",
+      "lwwwwwwwwr",
+    ],
+  ];
+
+  // add map level 
+  map = addLevel(matrix[level], mapConfig);
 });
 
-go("play");
+go("play", { level: 0 });
